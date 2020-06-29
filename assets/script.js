f36ef53d403cbc9ec6012a0bb8d6216e13968a36
Leo Ayala <leo_a1016@yahoo.com>
2020-06-28 23:47:33 -0400

    reorganized code for easier reading

--- assets/script.js
+++ assets/script.js
@@ -2,45 +2,44 @@ var timerButtonEl = document.getElementById('Tbutton');
 var startBtn = document.getElementById("start")
 var highScoreBtn = document.getElementById('highScore')
 var GameEl = document.querySelector("#gamePage")
+var QuestionEL = document.querySelector("#quesPosition")
+var AnswerEl = document.querySelector("#AnsPosition")
 
-var countDown = function () {
-    var timeLeft = 10;
+    var countDown = function () {
+        var timeLeft = 5;
+        //small timer for testing
 
-var timeInterval = setInterval(function() {
-    if (timeLeft > 0 ) {
-        timerButtonEl.textContent = timeLeft;
-        timeLeft--;
-    } else {
-        clearInterval(timeInterval);
-        alert("time is over!")
-    } 
-}, 1000);
-console.log(QuizArray)
-
-}
-
-var QuizArray = [
-    {q: 'event.preventDefault() stops the page from reseting when the page is refreshed.', a: 't'},
-    {q: '.getItem and .setItem are the way to save and load information in the LocalStorage.', a: 't'},
-    {q: 'JavaScript directly changes the original CSS file.', a: 'f'},
-    {q: 'LocalStorage can store values, objects, and strings.', a: 'f'},
-    {q: 'JSON.stringify allows you to convert [Objects] to strings for LocalStorage', a: 't'},
-]
-
-var startGame = function () {
-    for (i = 0; i < QuizArray.length; i++)
-    console.log(QuizArray)
-}
-
-
-var scorePage = function() {
-    console.log("hello");
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 0 ) {
+            timerButtonEl.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            clearInterval(timeInterval);
+            alert("time is over!");
+        };
+    }, 1000);
+        startGame();
 };
-// create array of questions and answers
-// create div element in JS to house question/answer
-// dynamically change div to questions/answers 
-// code if the answer is write or wrong/record for score
-// ability to add score and initials to high score page
 
-startBtn.addEventListener("click", countDown, startGame);
-highScoreBtn.addEventListener("click", scorePage)
+    var QuizArray = [
+        {q: 'event.preventDefault() stops the page from reseting when the page is refreshed.', a: 'true'},
+        {q: '.getItem and .setItem are the way to save and load information in the LocalStorage.', a: 'true'},
+        {q: 'JavaScript directly changes the original CSS file.', a: 'false'},
+        {q: 'LocalStorage can store values, objects, and strings.', a: 'false'},
+        {q: 'JSON.stringify allows you to convert [Objects] to strings for LocalStorage', a: 'true'},
+];
+
+    var startGame = function () {
+        for (i = 0; i < QuizArray.length; i++);
+        console.log(QuizArray);
+        //create dynamic element and place array questions in them
+};
+
+
+    var scorePage = function() {
+        console.log("hello");
+        // ability to add score and initials to high score page
+};
+
+startBtn.addEventListener("click", countDown);
+highScoreBtn.addEventListener("click", scorePage);
